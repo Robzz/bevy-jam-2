1ffcd0556489782cf681400e09f1ce4804babbad
LaCreArthur <le-tutur@hotmail.fr>
2022-08-25 22:43:56 +0200

    window name 😈

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ mod util;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Lost Portal Prototype v.0.666".to_string(),
+            width: 1280.,
+            height: 720.,
+            ..Default::default()
+        })
         .add_plugin(plugins::game::GamePlugin)
         .run();
 }
